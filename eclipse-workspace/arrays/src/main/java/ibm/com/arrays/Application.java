026d6c758e2cff1f5f45ca72a86bf6fd632fad99
Rishek kumar <rishek26kumar@gmail.com>
2025-04-29 14:29:41 +0530

    point diamensional array

--- eclipse-workspace/arrays/src/main/java/ibm/com/arrays/Application.java
+++ eclipse-workspace/arrays/src/main/java/ibm/com/arrays/Application.java
@@ -1,21 +1,30 @@
 package ibm.com.arrays;
+class Point{
+	public int x;
+	public int y;
+}
 
 public class Application
 {
 	public static void main( String[] args )
 	{
+		Point points[];
+		points=new Point[3];
+		points[0]=new Point();
+		points[0].x=3;
 //		int nums[];
 //		nums=new int[5];
 //		for(int value:nums) {
 //			System.out.println(value);
 //		}
-		int nums[][];
-		nums=new int[2][5];
-		for(int value[]:nums) {
-			for(int digit:value) {
-				System.out.println(digit);
-			}
-			System.out.println();
-		}
+//		int nums[][];
+//		nums=new int[2][5];
+//		for(int value[]:nums) {
+//			for(int digit:value) {
+//				System.out.println(digit);
+//			}
+//			System.out.println();
+//		}
+		
 	}
 }

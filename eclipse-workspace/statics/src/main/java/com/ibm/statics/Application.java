09c922dec05670b7289180543be2ec7428cd3759
Rishek kumar <rishek26kumar@gmail.com>
2025-04-29 16:46:51 +0530

    static initializer

--- eclipse-workspace/statics/src/main/java/com/ibm/statics/Application.java
+++ eclipse-workspace/statics/src/main/java/com/ibm/statics/Application.java
@@ -1,19 +1,38 @@
 package com.ibm.statics;
+
 class Data{
 	static public int value;
-	
+	public int getValue() {
+		return value;
+	}
+	{
+		System.out.println("INline");
+	}
+	static {// Static Initializer
+		value=10;
+		System.out.println("Static Initializer executed");
+	}
+	public Data(){
+		System.out.println("Constructer");
+	}
 }
+
 
 public class Application
 {
+//	int count=100;
 	public static void main( String[] args )
 	{
-		Data data1 = new data1();
-		Data data2 = new data2();
-		data1.value=3;
-		data2.value=5;
-		Data.value=77;
-		System.out.println(data1.value+"\t"+data2.value);
+//		Application application= new Application();
+//		System.out.println(application.count);
+		
+//		Data data1 = new Data();
+//		Data data2 = new Data();
+//		data1.value=3;
+//		data2.value=5;
+//		Data.value=77;
+		new Data();
+//		System.out.println(data1.value+"\t"+data2.value);
 				
 	}
 }

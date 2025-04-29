3ce27a4fee0a28e35e6fa12d34c15758eaf1b73f
Rishek kumar <rishek26kumar@gmail.com>
2025-04-29 10:41:27 +0530

    started with strings

---
+++ eclipse-workspace/maven-project/src/main/java/mavenproject/javaApp.java
@@ -0,0 +1,10 @@
+package mavenproject;
+
+public class javaApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Runtime runtime = Runtime.getRuntime();
+	}
+
+}

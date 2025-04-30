b2ebed190ccaa0cab207c4bdbcc3e3edc2bfedbf
Rishek kumar <rishek26kumar@gmail.com>
2025-04-30 10:09:59 +0530

    Adding cost of apple

--- eclipse-workspace/enums/src/main/java/com/ibm/enums/Application.java
+++ eclipse-workspace/enums/src/main/java/com/ibm/enums/Application.java
@@ -1,17 +1,36 @@
 package com.ibm.enums;
+
 enum Apple{
-	GoldenDel,
-	RedDel,
-	Gala
+	GoldenDel(10),
+	RedDel(15),
+	Gala(20);
+	private int price;
+	Apple(int cost){
+		price=cost;
+	}
+	public int getPrice() {
+		return price;
+	}
 }
+
 
 public class Application
 {
 	public static void main( String[] args )
 	{
+//		Apple apple = Apple.RedDel;
+//		System.out.println(apple);
+//		
+//		apple = Apple.valueOf("Gala");
+//		System.out.println(apple);
+		
+//		Apple[] apples= Apple.values();
+//		for (Apple apple: apples) {
+//			System.out.println(apple);
+//		}
 		Apple[] apples= Apple.values();
 		for (Apple apple: apples) {
-			System.out.println(apple);
+			System.out.println(apple+" costs "+apple.getPrice());
 		}
 	}
 }

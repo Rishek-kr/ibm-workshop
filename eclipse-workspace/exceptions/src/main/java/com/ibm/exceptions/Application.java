f3b0ba57cc6586303935025bd629c7cb1d3bc17f
Rishek kumar <rishek26kumar@gmail.com>
2025-04-30 17:14:22 +0530

    exception try catch with multiple catch

--- eclipse-workspace/exceptions/src/main/java/com/ibm/exceptions/Application.java
+++ eclipse-workspace/exceptions/src/main/java/com/ibm/exceptions/Application.java
@@ -6,8 +6,14 @@ public class Application
 	{
 		System.out.println("Start");
 		try {
-			System.out.println(args[0]);
+			System.out.println(Integer.parseInt(args[0])/Integer.parseInt(args[1]));
 		}catch(ArrayIndexOutOfBoundsException ex){
+			System.out.println("Run the program supplying a number");
+		}
+		catch(ArithmeticException ex){
+			System.out.println("Run the program supplying a number");
+		}
+		catch(NumberFormatException ex){
 			System.out.println("Run the program supplying a number");
 		}
 		System.out.println("End");

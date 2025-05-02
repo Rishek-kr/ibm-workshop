cd4f1046a3c49c4cd6022dc3672a9b937f70bcf6
Rishek kumar <rishek26kumar@gmail.com>
2025-05-02 11:21:40 +0530

    nested try block

--- eclipse-workspace/exceptions/src/main/java/com/ibm/exceptions/Application.java
+++ eclipse-workspace/exceptions/src/main/java/com/ibm/exceptions/Application.java
@@ -4,9 +4,18 @@ public class Application
 {
 	public static void main( String[] args )
 	{
+		String firstPara=null;
+		String secondPara=null;
 		System.out.println("Start");
 		try {
-			System.out.println(Integer.parseInt(args[0])/Integer.parseInt(args[1]));
+			firstPara=args[0];
+			secondPara=args[1];
+			try {
+				System.out.println(Integer.parseInt(firstPara)+Integer.parseInt(secondPara));
+			}catch(NumberFormatException ex){
+				System.out.println("Nested try block");
+			}
+			System.out.println("End of outer try block");
 		}
 //		}catch(ArrayIndexOutOfBoundsException ex){
 //			System.out.println("Run the program supplying a number");
@@ -20,10 +29,10 @@ public class Application
 //		catch(NumberFormatException ex){
 //			System.out.println("Run the program supplying a number");
 //		}
-//		catch(Exception ex){
-//			System.out.println("Run the program supplying a number");
-//			exception.
-//		}
+		catch(Exception exception){
+			System.out.println("Enter correct input");
+			exception.printStackTrace();
+		}
 		System.out.println("End");
 	}
 }

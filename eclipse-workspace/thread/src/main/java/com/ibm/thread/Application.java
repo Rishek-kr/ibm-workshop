081ec69630c755062c64ff134cd9ba1064757ea7
Rishek kumar <rishek26kumar@gmail.com>
2025-05-02 16:01:19 +0530

    thread using lamda fuction

--- eclipse-workspace/thread/src/main/java/com/ibm/thread/Application.java
+++ eclipse-workspace/thread/src/main/java/com/ibm/thread/Application.java
@@ -1,21 +1,27 @@
 package com.ibm.thread;
 
-class Runner implements Runnable{
-	
-	@Override
-	public void run() {
+//class Runner implements Runnable{
+//	
+//	@Override
+//	public void run() {
+//		for(int c=0;c<10;c++) {
+//			System.out.println(Thread.currentThread()+"--->"+String.valueOf(c));
+//		}
+//	}
+//}
+
+public class Application
+{
+	public static void workLoad() {
 		for(int c=0;c<10;c++) {
 			System.out.println(Thread.currentThread()+"--->"+String.valueOf(c));
 		}
 	}
-}
-
-public class Application
-{
 	public static void main( String[] args )
 	{
-		Runner runner = new Runner();
-		Thread thread1= new Thread(runner);
+//		Runner runner = new Runner();
+//		Thread thread1= new Thread(runner);
+		Thread thread1 = new Thread(()->workLoad());
 		System.out.println(thread1.getState());
 		thread1.start();
 		System.out.println(thread1.getState());

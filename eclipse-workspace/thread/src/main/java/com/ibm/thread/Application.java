41461901db16291790e97c7e151ce5bbfaf03556
Rishek kumar <rishek26kumar@gmail.com>
2025-05-02 15:30:11 +0530

    thread

---
+++ eclipse-workspace/thread/src/main/java/com/ibm/thread/Application.java
@@ -0,0 +1,29 @@
+package com.ibm.thread;
+
+class Runner implements Runnable{
+	
+	@Override
+	public void run() {
+		for(int c=0;c<10;c++) {
+			System.out.println(Thread.currentThread()+"--->"+String.valueOf(c));
+		}
+	}
+}
+
+public class Application
+{
+	public static void main( String[] args )
+	{
+		Runner runner = new Runner();
+		Thread thread1= new Thread(runner);
+		System.out.println(thread1.getState());
+		thread1.start();
+		System.out.println(thread1.getState());
+		try {
+			Thread.sleep(100);
+		} catch(InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println(thread1.getState());
+	}
+}

84a33996ed1100c58b3abbc3e55226eec557429b
Rishek kumar <rishek26kumar@gmail.com>
2025-05-06 09:15:28 +0530

    threadpool schedule

--- eclipse-workspace/threadpool/src/main/java/com/ibm/threadpool/Application.java
+++ eclipse-workspace/threadpool/src/main/java/com/ibm/threadpool/Application.java
@@ -1,24 +1,31 @@
 package com.ibm.threadpool;
 
+import java.util.Date;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class Application
 {
 	public static void main( String[] args )
 	{
-//		int vCPUs = Runtime.getRuntime().availableProcessors();
-//		System.out.println(vCPUs);
-		ExecutorService service = Executors.newCachedThreadPool();
-		for(int counter=0;counter<10;counter++) {
-			service.execute(new Task(counter));
-			try {
-				Thread.sleep(10);
-			} catch (InterruptedException e) {
-				// TODO: handle exception
-				e.printStackTrace();
-			}
-		}
+		int vCPUs = Runtime.getRuntime().availableProcessors();
+		System.out.println(vCPUs);
+		ScheduledExecutorService service = Executors.newScheduledThreadPool(vCPUs);
+//		for(int counter=0;counter<10;counter++) {
+//			service.execute(new Task(counter));
+//			try {
+//				Thread.sleep(10);
+//			} catch (InterruptedException e) {
+//				// TODO: handle exception
+//				e.printStackTrace();
+//			}
+//		}
+		service.schedule(new Task(1), 10, TimeUnit.SECONDS);
+		System.out.println(new Date());
+		service.scheduleAtFixedRate(new Task(2), 10, 5, TimeUnit.SECONDS);
+		service.scheduleWithFixedDelay(new Task(3), 10, 5, TimeUnit.SECONDS);
 	}
 	static class Task implements Runnable{
 		int count;

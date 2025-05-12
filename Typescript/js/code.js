e7ba4bc6d01de9bd907d4a7ca442f179ad0acdea
Rishek kumar <rishek26kumar@gmail.com>
2025-05-12 11:44:56 +0530

    function in typescript

---
+++ Typescript/js/code.js
@@ -0,0 +1,12 @@
+firstFuction();
+secondFuction();
+var firstFuction;
+function firstFuction() {
+    var firstName = "Rishek";
+    console.log(firstName);
+}
+function secondFuction() {
+    var firstName = "Aniket";
+    console.log(firstName);
+}
+console.log(firstName);
---
+++ Typescript/ts/code.ts
@@ -0,0 +1,12 @@
+firstFuction();
+secondFuction();
+let firstFuction :string | undefined;
+function firstFuction(){
+    let firstName="Rishek";
+    console.log(firstName);
+}
+function secondFuction(){
+    let firstName="Aniket";
+    console.log(firstName);
+}
+console.log(firstName);
